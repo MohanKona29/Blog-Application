ddd81be5095527751fa9e5bfbd8093d1262cd4f0
Mohan Kona <mohankona161@gmail.com>
2020-09-22 16:07:17 +0530

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :posts , dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :username, :email, :password , presence: true
-  validates :usrname, :email, uniqueness: true
+  validates :username, :email, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes" do
-  	expect(User.new(email: "ashish@gmail.com", password: "random")).to be_valid
-  end
+  context 'validation tests' do
+    it "ensures username presence" do
+    	user = User.new(email: "mohan@gmail.com", password: "qwerty").save
+      expect(user).to eq(false)
+    end
+
+    it "ensures email presence" do
+    	user = User.new(username: "Mohan", password: "qwerty").save
+      expect(user).to eq(false)
+    end
+
+    it "ensures password presence" do
+    	user = User.new(username: "Mohan", email: "mohan@gmail.com").save
+      expect(user).to eq(false)
+    end
+
+    it "should save successfully" do
+    	user = User.new(username: "Mohan", email:"Mohan@gmail.com", password: "qwerty").save
+      expect(user).to eq(true)
+    end
+end
 end
